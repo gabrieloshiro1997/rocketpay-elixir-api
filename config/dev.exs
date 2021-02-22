337b5c548b0cb568a6ac35beefd0daf86910029f
Gabriel Oshiro <gabrieloshiro1997@gmail.com>
2021-02-22 19:37:49 -0300

    chore: add database credentials

--- config/dev.exs
+++ config/dev.exs
@@ -3,9 +3,10 @@ use Mix.Config
 # Configure your database
 config :rocketpay, Rocketpay.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "docker",
   database: "rocketpay_dev",
   hostname: "localhost",
+  port: 5433,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
